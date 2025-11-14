224b3d1db161763cd3b56bf10b8a3f625ac232f5
BrunoAmrlS <bruno.amrl.s@gmail.com>
2025-11-13 23:29:01 -0300

    Adiciona suporte para criar índice FAISS

--- backend/create_knowledge_base.py
+++ backend/create_knowledge_base.py
@@ -1,10 +1,11 @@
 import os
 import base64
 
-# Cria a pasta se não existir
-os.makedirs('base_conhecimento', exist_ok=True)
 
-# Reconstrói o PDF
+os.makedirs('base_conhecimento', exist_ok=True)
+os.makedirs('faiss_index', exist_ok=True)
+
+
 pdf_content = os.environ.get('PDF_CONHECIMENTO')
 if pdf_content:
     pdf_bytes = base64.b64decode(pdf_content)
@@ -14,7 +15,6 @@ if pdf_content:
 else:
     print("❌ Variável PDF_CONHECIMENTO não encontrada!")
 
-# Reconstrói o TXT
 txt_content = os.environ.get('TXT_SOBRE_PORTFOLIO')
 if txt_content:
     txt_decoded = base64.b64decode(txt_content).decode('utf-8')
@@ -23,3 +23,23 @@ if txt_content:
     print("✅ TXT criado com sucesso!")
 else:
     print("❌ Variável TXT_SOBRE_PORTFOLIO não encontrada!")
+
+
+faiss_content = os.environ.get('FAISS_INDEX')
+if faiss_content:
+    faiss_bytes = base64.b64decode(faiss_content)
+    with open('faiss_index/index.faiss', 'wb') as f:
+        f.write(faiss_bytes)
+    print("✅ index.faiss criado com sucesso!")
+else:
+    print("❌ Variável FAISS_INDEX não encontrada!")
+
+
+pkl_content = os.environ.get('FAISS_PKL')
+if pkl_content:
+    pkl_bytes = base64.b64decode(pkl_content)
+    with open('faiss_index/index.pkl', 'wb') as f:
+        f.write(pkl_bytes)
+    print("✅ index.pkl criado com sucesso!")
+else:
+    print("❌ Variável FAISS_PKL não encontrada!")
